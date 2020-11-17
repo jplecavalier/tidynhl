cb57fb09ebc21ccfc0640d15a3bdbedcc9d8f5df
J.P. Le Cavalier <jplecavalier@me.com>
2020-11-16 23:27:36 -0500

    Minimal tests for get_players_id()

---
+++ tests/testthat/test-test-get-players-id.R
@@ -0,0 +1,50 @@
+test_that("get_players_id() works using only players_name", {
+
+  # Single element
+  expect_equal(get_players_id("Maurice Richard"), 8448321L)
+
+  # Vectorized
+  expect_equal(
+    object   = get_players_id(c("Saku Koivu", "Martin St-Louis", "Martin St. Louis")),
+    expected = c(8459442L, 8466378L, 8466378L)
+  )
+
+})
+
+test_that("get_players_id() works using players_name and players_dob", {
+
+  # Single element
+  expect_equal(get_players_id(
+    players_name = "Maurice Richard",
+    players_dob  = as.Date("1921-08-04")
+  ), 8448321L)
+
+  # Vectorized
+  expect_equal(
+    object   = get_players_id(
+      players_name = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
+      players_dob  = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18"))
+    ),
+    expected = c(8459442L, 8466378L, 8466378L)
+  )
+
+})
+
+test_that("get_players_id() works using players_name and players_position_type", {
+
+  # Single element
+  expect_equal(get_players_id(
+    players_name           = "Maurice Richard",
+    players_position_type  = "F"
+  ), 8448321L)
+
+  # Vectorized
+  expect_equal(
+    object   = get_players_id(
+      players_name          = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
+      players_position_type = c("F", "F", "F")
+    ),
+    expected = c(8459442L, 8466378L, 8466378L)
+  )
+
+})
