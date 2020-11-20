5f03ad4d9afa14d5cfed607d88a3171a10c238d0
J.P. Le Cavalier <jplecavalier@me.com>
2020-11-20 11:39:27 -0500

    Full tests for get_players_id()

--- tests/testthat/test-get-players-id.R
+++ tests/testthat/test-get-players-id.R
@@ -48,3 +48,38 @@ test_that("get_players_id() works using players_name and players_position_type",
   )
 
 })
+
+test_that("get_players_id() works using all parameters", {
+
+  # Single element
+  expect_equal(get_players_id(
+    players_name           = "Maurice Richard",
+    players_dob            = as.Date("1921-08-04"),
+    players_position_type  = "F"
+  ), 8448321L)
+
+  # Vectorized
+  expect_equal(
+    object   = get_players_id(
+      players_name          = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
+      players_dob           = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18")),
+      players_position_type = c("F", "F", "F")
+    ),
+    expected = c(8459442L, 8466378L, 8466378L)
+  )
+
+})
+
+test_that("get_players_id() returns warning for no ID matching", {
+
+  expect_warning(out <- get_players_id("J.P. Le Cavalier"), "no ID")
+  expect_equal(out, NA_integer_)
+
+})
+
+test_that("get_players_id() returns warning for multiple IDs matching", {
+
+  expect_warning(out <- get_players_id("Sebastian Aho"), "multiple IDs")
+  expect_equal(out, 8480222L)
+
+})
