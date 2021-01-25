1533f6e23d9e1d768b1426247eccaddf3e54165b
J.P. Le Cavalier <jplecavalier@me.com>
2021-01-25 15:34:21 -0500

    Remove teams_info from internal data

--- data-raw/compile_internal_data.R
+++ data-raw/compile_internal_data.R
@@ -5,9 +5,8 @@ library(magrittr)
 library(stringr)
 library(tidynhl)
 
-source("data-raw/teams_info.R")
 source("data-raw/seasons_info.R")
 source("data-raw/prospects_info.R")
 source("data-raw/players_info.R")
 
-usethis::use_data(teams_info, seasons_info, prospects_info, players_info, internal=TRUE, overwrite=TRUE)
+usethis::use_data(seasons_info, prospects_info, players_info, internal=TRUE, overwrite=TRUE)
