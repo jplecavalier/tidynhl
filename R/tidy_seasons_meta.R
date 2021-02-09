1725b165270d9df86e121d726dd376b18b80bc5d
J.P. Le Cavalier <jplecavalier@me.com>
2021-02-09 14:25:33 -0500

    Finalize the tidy_seasons_meta() function

--- R/tidy_seasons_meta.R
+++ R/tidy_seasons_meta.R
@@ -50,8 +50,45 @@ tidy_seasons_meta <- function(
 
 load_seasons_meta <- function() {
 
-  setcolorder(seasons_meta)
-  setorder(seasons_meta)
+  seasons_meta <- create_data_table(get_stats_api("seasons")[[1]]$seasons)
+
+  validate_columns(seasons_meta, list(
+    seasonId = NA_character_,
+    regularSeasonStartDate = NA_character_,
+    regularSeasonEndDate = NA_character_,
+    seasonEndDate = NA_character_,
+    numberOfGames = NA_integer_,
+    tiesInUse = NA,
+    olympicsParticipation = NA,
+    conferencesInUse = NA,
+    divisionsInUse = NA,
+    wildCardInUse = NA
+  ))
+
+  seasons_meta <- seasons_meta[, .(
+    season_id = seasonId,
+    season_regular_start = as.Date(regularSeasonStartDate),
+    season_regular_end = as.Date(regularSeasonEndDate),
+    season_playoffs_end = as.Date(seasonEndDate),
+    season_games_nb = numberOfGames,
+    season_ties = tiesInUse,
+    season_olympics = olympicsParticipation,
+    season_conferences = conferencesInUse,
+    season_divisions = divisionsInUse,
+    season_wildcards = wildCardInUse
+  )]
+
+  teams_meta <- tidy_teams_meta(active_only = FALSE, keep_id = TRUE, return_datatable = TRUE)
+  season_active_id <- seasons_meta[, max(season_id)]
+  teams_meta[team_active == TRUE, season_last_id := season_active_id]
+
+  seasons_meta[, season_teams_nb := sapply(season_id, function(season_id) {
+    teams_meta[season_first_id <= season_id & season_id <= season_last_id, .N]
+  })]
+  seasons_meta[season_id == "19311932", season_teams_nb := season_teams_nb -1L]
+
+  setcolorder(seasons_meta, c("season_id", "season_regular_start", "season_regular_end",
+                              "season_playoffs_end", "season_teams_nb", "season_games_nb"))
 
   assign("seasons_meta", seasons_meta, data)
 
