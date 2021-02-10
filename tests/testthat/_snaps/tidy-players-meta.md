ccc45767827a62d4f2aeb84b6279282c78197956
J.P. Le Cavalier <jplecavalier@me.com>
2021-02-09 19:43:58 -0500

    Adjust existing tests to refactoring

--- tests/testthat/_snaps/tidy-players-meta.md
+++ tests/testthat/_snaps/tidy-players-meta.md
@@ -1,15 +1,23 @@
 # tidy_players_meta() gives correct informations
 
-        player_name player_birth_date player_birth_city player_birth_stateprovince
-    1   Carey Price        1987-08-16       Anahim Lake                         BC
-    2 Mario Lemieux        1965-10-05          Montreal                         QC
-    3   Al MacInnis        1963-07-11         Inverness                         NS
-      player_birth_country player_nationality player_inches player_pounds
-    1                  CAN                CAN            75           217
-    2                  CAN                CAN            76           230
-    3                  CAN                CAN            73           204
-      player_hand player_position player_position_type
-    1           L               G                    G
-    2           R               C                    F
-    3           R               D                    D
+         player_name player_active player_roster_status player_number
+    1: Mario Lemieux         FALSE                    N            66
+    2:   Al MacInnis         FALSE                    I             2
+    3:   Carey Price          TRUE                    Y            31
+       player_position player_position_type player_nationality player_birth_country
+    1:               C                    F                CAN                  CAN
+    2:               D                    D                CAN                  CAN
+    3:               G                    G                CAN                  CAN
+       player_birth_stateprovince player_birth_city player_birth_date player_dead
+    1:                         QC          Montreal        1965-10-05       FALSE
+    2:                         NS         Inverness        1963-07-11       FALSE
+    3:                         BC       Anahim Lake        1987-08-16       FALSE
+       player_death_date player_inches player_pounds player_hand player_rookie
+    1:              <NA>            76           230           R         FALSE
+    2:              <NA>            73           204           R         FALSE
+    3:              <NA>            75           220           L         FALSE
+       player_hof player_hof_year player_nhl_100 team_abbreviation
+    1:       TRUE            1997           TRUE              <NA>
+    2:       TRUE              NA           TRUE              <NA>
+    3:      FALSE              NA          FALSE               MTL
 
--- tests/testthat/test-get-players-id.R
+++ tests/testthat/test-get-players-id.R
@@ -5,7 +5,7 @@ test_that("get_players_id() works using only players_name", {
 
   # Vectorized
   expect_equal(
-    object   = get_players_id(c("Saku Koivu", "Martin St-Louis", "Martin St. Louis")),
+    object = get_players_id(c("Saku Koivu", "Martin St-Louis", "Martin St. Louis")),
     expected = c(8459442L, 8466378L, 8466378L)
   )
 
@@ -16,14 +16,14 @@ test_that("get_players_id() works using players_name and players_dob", {
   # Single element
   expect_equal(get_players_id(
     players_name = "Maurice Richard",
-    players_dob  = as.Date("1921-08-04")
+    players_dob = as.Date("1921-08-04")
   ), 8448321L)
 
   # Vectorized
   expect_equal(
-    object   = get_players_id(
+    object = get_players_id(
       players_name = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
-      players_dob  = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18"))
+      players_dob = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18"))
     ),
     expected = c(8459442L, 8466378L, 8466378L)
   )
@@ -34,14 +34,14 @@ test_that("get_players_id() works using players_name and players_position_type",
 
   # Single element
   expect_equal(get_players_id(
-    players_name           = "Maurice Richard",
-    players_position_type  = "F"
+    players_name = "Maurice Richard",
+    players_position_type = "F"
   ), 8448321L)
 
   # Vectorized
   expect_equal(
-    object   = get_players_id(
-      players_name          = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
+    object = get_players_id(
+      players_name = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
       players_position_type = c("F", "F", "F")
     ),
     expected = c(8459442L, 8466378L, 8466378L)
@@ -53,16 +53,16 @@ test_that("get_players_id() works using all parameters", {
 
   # Single element
   expect_equal(get_players_id(
-    players_name           = "Maurice Richard",
-    players_dob            = as.Date("1921-08-04"),
-    players_position_type  = "F"
+    players_name = "Maurice Richard",
+    players_dob = as.Date("1921-08-04"),
+    players_position_type = "F"
   ), 8448321L)
 
   # Vectorized
   expect_equal(
-    object   = get_players_id(
-      players_name          = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
-      players_dob           = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18")),
+    object = get_players_id(
+      players_name = c("Saku Koivu", "Martin St-Louis", "Martin St. Louis"),
+      players_dob = as.Date(c("1974-11-23", "1975-06-18", "1975-06-18")),
       players_position_type = c("F", "F", "F")
     ),
     expected = c(8459442L, 8466378L, 8466378L)
